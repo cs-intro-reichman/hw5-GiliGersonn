0f9d2a83c250331ccaefba44bc7cb572cbf2225b
Gili Gerson <gili.gerson@post.runi.ac.il>
2025-11-29 23:55:32 +0200

    test5

--- Wordle.java
+++ Wordle.java
@@ -132,8 +132,8 @@ public class Wordle {
                 }
             }
 
-            // Store guess and compute feedback
-            // ... use storeGuess and computeFeedback
+            storeGuess(guess, guesses, attempt);
+            computeFeedback(secret, guess, results[attempt]);
 
             // Print board
             printBoard(guesses, results, attempt);
